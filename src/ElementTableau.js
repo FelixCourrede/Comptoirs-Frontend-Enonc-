5cc717e3e14211af96770c7d10803a7fb3738b6d
FelixCourrede <116156131+FelixCourrede@users.noreply.github.com>
2023-02-06 10:18:27 +0100

    Des trucs

---
+++ src/ElementTableau.js
@@ -0,0 +1,7 @@
+export default class ElementTableau {
+    constructor (Nom, Prix, Stock, Commandes = false) {
+        this._texte = texte;
+        this._fait = fait;
+        this._id = Date.now() + Math.floor(Math.random()*10) // génération d'un id
+        }
+    }
---
+++ src/components/tableau.vue
@@ -0,0 +1,38 @@
+<script setup>
+    function getCat(){
+        const url='https://springajax.herokuapp.com/api';
+        let fetchOptions = { method: "GET"};
+        fetch(url, fetchOptions)
+        .then((response) => {
+           console.log(response)
+           return response.json();
+        })
+        .then((datajson) => {
+            console.log(datajson)
+            let categories=datajson;
+            console.log(categories)
+            let html="";
+            for(let k of categories){
+                html+='<tr><td>'+categories.libelle+'</td><td>'+categories.description+'</td></tr>';
+            }
+            document.getElementById('corps').innerHTML=html;
+        })
+    }
+    getCat();
+</script>
+
+<template>
+    <table>
+        <thead>
+            <tr>
+                <th>Nom</th>
+                <th>Prix</th>
+                <th>Stock</th>
+                <th>Commandés</th>
+            </tr>
+        </thead>
+        <tbody id="corps">
+
+        </tbody>
+    </table>
+</template>
